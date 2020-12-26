4197c63027d4a2fe79e9d2d76115019e12a67187
kotard8899 <sally41176789@yahoo.com.tw>
2020-12-26 18:10:46 +0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express'); // node 不支援 import from (至少這堂課的時候)
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');  // 沒export東西，所以不用賦值
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({ hi: 'there' });
-});
-const port = process.env.PORT || 5000
-app.listen(port);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
